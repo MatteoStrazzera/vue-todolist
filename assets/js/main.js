b20e4cb24007c4958c17d818a859f4c702b6a257
MatteoStrazzera <mattestrazz@gmail.com>
2024-03-13 15:25:27 +0100

    add todolist in js and ul element in html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,36 @@
 console.log('connesso');
+
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        toDoList: [
+            {
+                text: 'Fare la spesa',
+                done: true
+            },
+            {
+                text: 'Preparare la cena',
+                done: true
+            },
+            {
+                text: 'Andare in palestra',
+                done: false
+            },
+            {
+                text: 'Buttare la spazzatura',
+                done: true
+            },
+            {
+                text: 'Fare gli esercizi',
+                done: false
+            },
+            {
+                text: 'Dormire la notte',
+                done: false
+            },
+        ]
+      }
+    }
+  }).mount('#app')
