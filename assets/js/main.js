c0d8a073b5f8d29a95d0a21d772d23a89a0f9abd
MatteoStrazzera <mattestrazz@gmail.com>
2024-03-13 23:11:36 +0100

    add milestone 3 with input, button and function to create new element and push in array/list

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,44 +2,52 @@ console.log('connesso');
 
 const { createApp } = Vue
 
-  createApp({
+createApp({
     data() {
-      return {
-        toDoList: [
-            {
-                text: 'Fare la spesa',
-                done: true
-            },
-            {
-                text: 'Preparare la cena',
-                done: true
-            },
-            {
-                text: 'Andare in palestra',
-                done: false
-            },
-            {
-                text: 'Buttare la spazzatura',
-                done: true
-            },
-            {
-                text: 'Fare gli esercizi',
-                done: false
-            },
-            {
-                text: 'Dormire la notte',
-                done: false
-            },
-        ]
-      }
-    },
-    methods: {
-        deleteToDo(index){
-            console.log('eliminato');
-            this.toDoList.splice(index, 1)
+        return {
+            toDoList: [
+                {
+                    text: 'Fare la spesa',
+                    done: true
+                },
+                {
+                    text: 'Preparare la cena',
+                    done: true
+                },
+                {
+                    text: 'Andare in palestra',
+                    done: false
+                },
+                {
+                    text: 'Buttare la spazzatura',
+                    done: true
+                },
+                {
+                    text: 'Fare gli esercizi',
+                    done: false
+                },
+                {
+                    text: 'Dormire la notte',
+                    done: false
+                },
+            ]
         }
     },
-    mounted(){
-        console.log(this.toDoList);
+    methods: {
+        // funzione per cancellare gli elementi della lista
+        deleteToDo(index) {
+            console.log('eliminato');
+            this.toDoList.splice(index, 1)
+        },
+        // funzione per creare un nuovo oggetto e inserirlo nella lista
+        addToDo() {
+            if (this.newToDo.trim() !== '') {
+                this.toDoList.push({ text: this.newToDo, done: false });
+                this.newToDo = '';
+            }
+        },
+        mounted() {
+            console.log(this.toDoList);
+        }
     }
-  }).mount('#app')
+}).mount('#app')
